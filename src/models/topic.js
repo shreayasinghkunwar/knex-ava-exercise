24f5cdbd3d604c28576649f08a27c3c2a592deaa
mrdivinemaniac <divinemaniac@gmail.com>
2022-09-13 21:54:56 +0545

    Create models for topic and week

---
+++ src/models/topic.js
@@ -0,0 +1,36 @@
+const { knex } = require('../database');
+const { WEEK_TABLE_NAME } = require('./week');
+
+const TOPIC_TABLE_NAME = 'topics';
+
+function insertForWeek (weekNumber, topicName) {
+  return knex(TOPIC_TABLE_NAME)
+    .insert({ week_number: weekNumber, name: topicName })
+    .returning('*');
+}
+
+function searchTopicWithWeekData (searchTerm) {
+  return knex(TOPIC_TABLE_NAME)
+    .select(`${TOPIC_TABLE_NAME}.*`)
+    .select(`${WEEK_TABLE_NAME}.name as week_name`)
+    .leftJoin(
+      WEEK_TABLE_NAME,
+      `${TOPIC_TABLE_NAME}.week_number`,
+      `${WEEK_TABLE_NAME}.number`
+    ).where(
+      `${TOPIC_TABLE_NAME}.name`, 'LIKE', `%${searchTerm}%`
+    );
+}
+
+/**
+ * @param {string} id
+ * @param {{ name?: string, week_number?: number }} updateData
+ */
+function updateTopicById (id, updateData) {
+  return knex(TOPIC_TABLE_NAME)
+    .update(updateData)
+    .where({ id })
+    .returning('*')
+}
+
+module.exports = { insertForWeek, searchTopicWithWeekData, updateTopicById }
---
+++ src/models/week.js
@@ -0,0 +1,34 @@
+const { knex } = require('../database');
+
+const WEEK_TABLE_NAME = 'weeks';
+
+function getTableQueryBuilder () {
+  return knex(WEEK_TABLE_NAME);
+}
+
+function insertWeek (number, name) {
+  return getTableQueryBuilder()
+    .insert({ number, name })
+    .returning('*');
+}
+
+/**
+ * @param {{ number: number, name: string }[]} weeks
+ */
+function insertWeeks (weeks) {
+  return getTableQueryBuilder()
+    .insert(weeks)
+    .returning('*');
+}
+
+function findAllWeeks () {
+  return getTableQueryBuilder()
+    .select(`${WEEK_TABLE_NAME}.*`)
+}
+
+module.exports = {
+  insertWeek,
+  findAllWeeks,
+  insertWeeks,
+  WEEK_TABLE_NAME
+};

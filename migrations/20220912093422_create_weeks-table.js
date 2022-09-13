fd116029dfa1b059d416a43fa60be96fcf5fd3d3
mrdivinemaniac <divinemaniac@gmail.com>
2022-09-13 21:52:07 +0545

    Add migration for weeks table

---
+++ migrations/20220912093422_create_weeks-table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('weeks', t => {
+    t.integer('number').notNullable().primary();
+    t.string('name').notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('weeks');
+};
